a1a007dba456da94f2c847f61497732108abaf42
Isabelle <noelisabelle15@gmail.com>
2024-03-29 11:29:01 +0100

    ex 2.1 finish

--- 02.control-structures/2.1/app.js
+++ 02.control-structures/2.1/app.js
@@ -1,3 +1,11 @@
 (() => {
-    // YOUR CODE HERE
+    let age = prompt ("Please enter your age");
+    if (age >= 18)
+    {
+        alert ("You are a adult")
+    }
+    else
+    {
+        alert ("You are not yet an adult")
+    }
 })();
