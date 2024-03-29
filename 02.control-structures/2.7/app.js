22a50e41ec767e8eb45407f798ba1e30529e4d7e
Isabelle <noelisabelle15@gmail.com>
2024-03-29 14:26:26 +0100

    exercise 2.7

--- 02.control-structures/2.7/app.js
+++ 02.control-structures/2.7/app.js
@@ -1,3 +1,9 @@
 (() => {
-    // YOUR CODE HERE
+    let n = parseInt(prompt("Enter a value for n"));
+    let addition = 0;
+    for (i = 0; i < n; i++)
+    {
+        addition = addition + parseInt(prompt("Enter a value to add"));
+    }
+    alert(`your addition result is ${addition}`);
 })();
