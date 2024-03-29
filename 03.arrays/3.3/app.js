56985ba27b77481a9fc49a55c62f6330d9be8f0f
Isabelle <noelisabelle15@gmail.com>
2024-03-29 15:51:42 +0100

    exercise 3.3

--- 03.arrays/3.3/app.js
+++ 03.arrays/3.3/app.js
@@ -1,3 +1,9 @@
 (() => {
-    // YOUR CODE HERE
+    let strawHatPirate = ["Luffy", "Zoro", "Ussopp", "Nami", "Sanji", "Chopper", "Robin", "Franky"];
+    let newArray = [];
+    for (i = 0 ; i < strawHatPirate.length ; i++)
+    {
+        newArray.push(strawHatPirate[i]);
+    }
+    console.log(newArray);
 })();
