b3c7850d36f12250c61e7d5749bcf9646b5b455a
Isabelle <noelisabelle15@gmail.com>
2024-03-29 11:40:44 +0100

    2.2 finished

--- 02.control-structures/2.2/app.js
+++ 02.control-structures/2.2/app.js
@@ -1,3 +1,16 @@
 (() => {
-    // YOUR CODE HERE
+    let min = prompt ("Please enter a number");
+    let max = prompt ("please enter a second number higher than the first one");
+    let current = prompt ("please enter yet another number");
+    if (min > max)
+    {
+        if (current > min && current < max)
+        {
+            alert (current);
+        }
+    }
+    else
+    {
+        alert ("please check the message when you enter your second number");
+    }
 })();
