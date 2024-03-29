804e6bf032b038f58bbfd16ad8b363bd73b29dcd
Isabelle <noelisabelle15@gmail.com>
2024-03-29 10:31:06 +0100

    ex 1.0

---
+++ 01.intro/1.0/app.js
@@ -0,0 +1,9 @@
+(() => {
+    let number1 = 5;
+    let number2 = 6;
+    console.log (number1 + number2);
+    console.log (number1 - number2);
+    console.log (number1 * number2);
+    console.log (number1 / number2);
+    console.log (number2 % number1);
+})();
