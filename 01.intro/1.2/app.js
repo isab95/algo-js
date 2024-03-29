1ad8356026278f980189ac6491581c3d14c3bcae
Isabelle <noelisabelle15@gmail.com>
2024-03-29 10:45:24 +0100

    exercise 1.2

--- 01.intro/1.2/app.js
+++ 01.intro/1.2/app.js
@@ -1,3 +1,6 @@
 (() => {
-    // YOUR CODE HERE
+    let name = "Noël";
+    let firstname = "Isabelle";
+    let city = "Peruwelz";
+    console.log(`My name is ${firstname} ${name} and I live in ${city}`);
 })();
