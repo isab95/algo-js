973a15c4c10b5ea120429b618d4700bd4aae0c5f
Isabelle <noelisabelle15@gmail.com>
2024-03-29 15:42:34 +0100

    exercise 3.2

--- 03.arrays/3.1/app.js
+++ 03.arrays/3.1/app.js
@@ -1,3 +1,15 @@
 (() => {
-    // YOUR CODE HERE
+    let arr1 = [1, 2, 3, 4, 5];
+    let arr2 = [100, 101, 102];
+    console.log(addArray(arr1));
+    console.log(addArray(arr2));
 })();
+function addArray (arr)
+{
+    let sum = 0;
+    for (i = 0 ; i < arr.length ; i++)
+    {
+        sum = sum + arr[i];
+    }
+    return "la somme du tableau est " + sum;
+}
