0934286b69c2d080e8ce65079604069c3c3b5614
Isabelle <noelisabelle15@gmail.com>
2024-03-29 15:53:40 +0100

    duplicate file 3.3

---
+++ 03.arrays/3.3.1/app.js
@@ -0,0 +1,9 @@
+(() => {
+    let strawHatPirate = ["Luffy", "Zoro", "Ussopp", "Nami", "Sanji", "Chopper", "Robin", "Franky"];
+    let newArray = [];
+    for (i = 0 ; i < strawHatPirate.length ; i++)
+    {
+        newArray.push(strawHatPirate[i]);
+    }
+    console.log(newArray);
+})();
