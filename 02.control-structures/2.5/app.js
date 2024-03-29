27aae226c1b903fe90964f7f0592f85e55118985
Isabelle <noelisabelle15@gmail.com>
2024-03-29 13:53:52 +0100

    exercise 2.5

--- 02.control-structures/2.5/app.js
+++ 02.control-structures/2.5/app.js
@@ -1,3 +1,9 @@
 (() => {
-    // YOUR CODE HERE
+    let n = prompt ("What's your favorite number?")
+    while (!(n == 42))//utilise que 2 = au lieu de 3 car sinon j'ai une boucle infinie (ne sort pas de la boucle lorsque j'écris 42)
+    {
+        alert ("Are you sure?");
+        n = prompt ("What's your favorite number?");
+    }
+    alert ("it's mine too");
 })();
