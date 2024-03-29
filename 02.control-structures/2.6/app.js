040532905fb4b1a69edb02eb9c6c4f9285481fd0
Isabelle <noelisabelle15@gmail.com>
2024-03-29 14:01:38 +0100

    exercise 2.6

--- 02.control-structures/2.6/app.js
+++ 02.control-structures/2.6/app.js
@@ -1,3 +1,8 @@
 (() => {
-    // YOUR CODE HERE
+    let weekDay = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saterday", "Sunday"];
+    let num = prompt("Type a number between 1 and 7");
+    if (num < 8 && num > 0)
+    {
+        console.log(weekDay[num-1]);
+    }
 })();
