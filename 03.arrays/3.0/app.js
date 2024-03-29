a206d033f9f949a5e10ed505c0eb39d56d15e940
Isabelle <noelisabelle15@gmail.com>
2024-03-29 14:53:37 +0100

    exercise 3.0

--- 03.arrays/3.0/app.js
+++ 03.arrays/3.0/app.js
@@ -1,3 +1,7 @@
 (() => {
-    // YOUR CODE HERE
+    let student = ["adca09", "Adrien B", "Angel", "Arnaud VA", "Denis L", "Druart Stacy", "Dylan Feys", "..."];
+    for (let elem of student)
+    {
+        console.log(student[elem]);
+    }
 })();
