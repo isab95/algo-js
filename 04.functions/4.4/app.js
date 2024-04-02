909e63109a29f91dec3aeaf064aee8b0ed97e716
isab95 <noelisabelle15@gmail.com>
2024-04-02 16:13:44 +0200

    exercise 4.4

--- 04.functions/4.4/app.js
+++ 04.functions/4.4/app.js
@@ -1,3 +1,14 @@
 (() => {
-    // YOUR CODE HERE
 })();
+/**function that return a certain number of element from an array chosen randomly */
+function pickLearner (inputAr, n){
+    let chosen = [];
+    let index;
+    for (let i = 1 ; i <= n; i++)
+    {
+        index = math.floor(math.random() * inputAr.lenght);
+        chosen.push(index);
+        inputAr.splice(index,1);
+    }
+    return chosen;
+}
