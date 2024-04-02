35dc949afa2d6a16780ca5696ce1fa01f7722fab
isab95 <noelisabelle15@gmail.com>
2024-04-02 16:30:30 +0200

    exercise 4.5

--- 04.functions/4.5/app.js
+++ 04.functions/4.5/app.js
@@ -1,3 +1,11 @@
 (() => {
-    // YOUR CODE HERE
+    let A = [1, 5];
+    let B = [3, 2];
+    console.log(calcDistance(A, B));
 })();
+/**function that generate the distance between two point by their coordinate in 2D */
+function calcDistance (A, B){
+    let distance = (A[0]-B[0])*(A[1]-B[1])
+
+    return distance >= 0 ? distance : distance * (-1)
+}
