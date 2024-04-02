2fb2221805219ffe11bf5c7e97a88f7f49fe93b1
isab95 <noelisabelle15@gmail.com>
2024-04-02 10:37:20 +0200

    exercise 4.3

--- 04.functions/4.2/app.js
+++ 04.functions/4.2/app.js
@@ -1,4 +1,5 @@
 (() => {
+    /**function that r'eturn a random number between 1 anf 10 (both include) */
     let rand10 = () => {
         return Math.floor((Math.random() * 10) + 1);
     }
--- 04.functions/4.3/app.js
+++ 04.functions/4.3/app.js
@@ -1,3 +1,16 @@
 (() => {
-    // YOUR CODE HERE
+    let rand10 = () => {
+        return Math.floor((Math.random() * 10) + 1);
+    }
+    /**function that return a array of random number of a given lenght */
+    let multiRand = (n) => {
+        let tab = [];
+        for (let i = 0 ; i < n; i++)
+        {
+            tab.push(rand10());
+        }
+        return tab;
+    }
+    let n = prompt("Enter a value");
+    console.log(multiRand(n));
 })();
