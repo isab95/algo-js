95a93f06afcb2e0b5d5e4c75db4eb8aa8a6604c4
isab95 <noelisabelle15@gmail.com>
2024-04-02 09:53:49 +0200

    exercise 4.1

--- 04.functions/4.1/app.js
+++ 04.functions/4.1/app.js
@@ -1,3 +1,9 @@
 (() => {
-    // YOUR CODE HERE
+    /**function that return the size of a rectangle surface */
+    let calcSurface = (a, b) => {
+        return a*b;
+    }
+    let lenght = prompt("Enter the lenght of your rectangle");
+    let widht = prompt("Enter the widht of your rectangle");
+    alert (`the surface of your rectangle is ${calcSurface(lenght,widht)}`)
 })();
