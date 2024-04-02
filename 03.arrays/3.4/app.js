a748e99cf338f332363b8d4fda9cf499173dfa58
isab95 <noelisabelle15@gmail.com>
2024-04-02 09:28:40 +0200

    exercise 3.4

--- 03.arrays/3.4/app.js
+++ 03.arrays/3.4/app.js
@@ -1,3 +1,16 @@
 (() => {
-    // YOUR CODE HERE
+    let tab = [3, 5, 84, 65, 214, 75, 0, 43];
+    let min = tab[0];
+    let max = tab[0];
+    for (let i = 1 ; i < tab.length ; i++)
+    {
+        if (tab[i] < min)
+        {
+            min = tab[i];
+        }else if (tab[i] > max)
+        {
+            max = tab[i];
+        }
+    }
+    console.log(`The min value of the array is ${min} and the max is ${max}`);
 })();
