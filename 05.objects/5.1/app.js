872076bba8e25d1248339a16a0ecf290bbd355c3
isab95 <noelisabelle15@gmail.com>
2024-04-03 10:58:07 +0200

    exercise 5.1

--- 05.objects/5.1/app.js
+++ 05.objects/5.1/app.js
@@ -1,3 +1,14 @@
 (() => {
-    // YOUR CODE HERE
+    console.log(JSON.stringify(favSerie));
 })();
+function askTvSerie(){
+    let favSerie = {
+        Name: prompt("What is the name of your favorite tv series?") ,
+        ProdYear: prompt("When was it produce(year)?"),
+        CastMember: []
+    }
+    do {
+        favSerie.CastMember.push(prompt("Add a cast member"))
+    }while (prompt("Would you like to add another cast member (Y/N)?").toUpperCase() === "Y")
+    return favSerie;
+}
