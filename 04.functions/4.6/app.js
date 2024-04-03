ba03023b3a8dd12be9675299ec76c0d03aacd8e1
isab95 <noelisabelle15@gmail.com>
2024-04-03 09:36:34 +0200

    exercise 4.6

--- 04.functions/4.6/app.js
+++ 04.functions/4.6/app.js
@@ -1,3 +1,32 @@
 (() => {
     // YOUR CODE HERE
 })();
+/**
+ * function that return the factorial of a given number
+ * @param {number of wich you want the factorial} a 
+ * @returns result of the calculation
+ */
+function factorial (a) {
+    let factor = 1;
+    for (i = 2; i <= a; i++ ){
+        factor *= i;
+    }
+    return factor;
+}
+/**
+ * calculation of a factorial
+ * @param {number of wich you want the factorial} a 
+ * @param {increment} i 
+ * @returns 
+ */
+function factorial (a, i){
+    let factor = 1;
+    if (i <= a){
+        factor *= i;
+        if (i === a){
+            return factor;
+        }else{
+            factorial(a, (i+1));
+        }
+    }
+}
